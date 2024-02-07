70e62b95c3c3820a1882ce9448ca1eb9b54f5b58
NeilRow56 <neilrowland56@gmail.com>
2024-02-07 17:33:26 +0000

    Added NR starter amendments

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  // eslint: {
+  // //   Warning: This allows production builds to successfully complete even if
+  // //   your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
