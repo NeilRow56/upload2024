c7b5680261169e276f9e1b903b0d20b6de620853
NeilRow56 <neilrowland56@gmail.com>
2024-02-10 10:47:40 +0000

    Amendments to UploadButton for refresh, to load new image

--- src/components/cloudinary-album/UploadButton.tsx
+++ src/components/cloudinary-album/UploadButton.tsx
@@ -1,36 +1,41 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { Button } from '../ui/button'
 import { CldUploadButton } from 'next-cloudinary'
 
 type Props = {}
 
 const UploadButton = (props: Props) => {
+  const router = useRouter()
   return (
     <Button asChild>
-      <div className="flex w-[100px] gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6 text-white"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-          />
-        </svg>
-
-        <CldUploadButton
-          onUpload={(result: any) => {
-            // setImageId(result.info.public_id);
-          }}
-          uploadPreset="lygsc1ol"
-        />
-      </div>
+      <CldUploadButton
+        onUpload={(result: any) => {
+          setTimeout(() => {
+            router.refresh()
+          }, 1000)
+        }}
+        uploadPreset="lygsc1ol"
+      >
+        <div className="flex w-[100px] gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+        </div>
+      </CldUploadButton>
     </Button>
   )
 }
